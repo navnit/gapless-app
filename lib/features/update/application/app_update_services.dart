483ff818fb0942e0eb42906d4c57159bd04b436b
Navnit Durai <contact.navnit@gmail.com>
2026-07-23 23:57:02 +0530

    feat: wire update services into AppDependencies.production

---
+++ lib/features/update/application/app_update_services.dart
@@ -0,0 +1,7 @@
+import 'package:gapless/features/update/application/update_coordinator.dart';
+
+final class AppUpdateServices {
+  const AppUpdateServices({required this.coordinator});
+
+  final UpdateCoordinator coordinator;
+}
